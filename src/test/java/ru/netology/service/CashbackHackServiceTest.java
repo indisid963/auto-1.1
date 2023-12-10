7abb65ca0756798f4d1e86a15509f3515cff4a57
indi__sid <indisid.indisid@gmail.com>
2023-12-10 17:02:41 +0300

    completeTest

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,22 +1,24 @@
 package ru.netology.service;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 public class CashbackHackServiceTest {
 
-   @org.testng.annotations.Test
-public void testRemain() {
-  CashbackHackService  service = new CashbackHackService();
-  int amount = 1000;
-  int actual = service.remain(amount);
-  int expected = 0;
-  assertEquals(actual, expected);
-   }
-    @org.testng.annotations.Test
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
     public void testRemainWithCashback() {
-       CashbackHackService  service = new CashbackHackService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 500;
         int actual = service.remain(amount);
         int expected = 500;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
