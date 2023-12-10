49969c622f2032eb43861d5146a08d92c059997a
indi__sid <indisid.indisid@gmail.com>
2023-12-10 17:05:45 +0300

    ReformateCode

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,19 +1,21 @@
 package ru.netology.service;
 
 import static org.testng.Assert.assertEquals;
+
 public class CashbackHackServiceTest {
 
-   @org.testng.annotations.Test
-public void testRemain() {
-  CashbackHackService  service = new CashbackHackService();
-  int amount = 1000;
-  int actual = service.remain(amount);
-  int expected = 0;
-  assertEquals(actual, expected);
-   }
+    @org.testng.annotations.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
     @org.testng.annotations.Test
     public void testRemainWithCashback() {
-       CashbackHackService  service = new CashbackHackService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 500;
         int actual = service.remain(amount);
         int expected = 500;
