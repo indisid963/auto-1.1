f36fd965da0ed69dcf56d327b5bca00ecdf7cd24
indi__sid <indisid.indisid@gmail.com>
2023-12-10 16:32:36 +0300

    completeTest

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -1,7 +1,9 @@
 package ru.netology.service;
 
 public class CashbackHackService {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
     }
 }
